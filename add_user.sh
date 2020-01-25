dab92dcca715e7257744519e713b51dd7ddd3e8e
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-01-25 23:27:13 +0000

    add add_user.sh
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

---
+++ add_user.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+run() {
+    echo "$*"
+    "$@" || exit $?
+}
+
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+confirm "Add user $1? [y/n]" && {
+    run adduser $1
+    run usermod -aG docker $1
+
+    run cd /home/$1
+    run su -c "git clone https://github.com/tomereli/bashtools.git /home/$1/bashtools" $1
+    run su -c "cp /home/$1/bashtools/.gitconfig /home/$1/" $1
+    run su -c "cp /home/$1/bashtools/.bashrc /home/$1/" $1
+    run su -c "cp /home/$1/bashtools/.bashrc.user /home/$1/.bashrc.$1" $1
+    run su -c "cp /home/$1/bashtools/.netrc /home/$1/" $1
+    run su -c "cp /home/$1/bashtools/.tmux.conf /home/$1/" $1
+    run su -c "cp /home/$1/bashtools/git-proxy /home/$1/" $1
+    run su -c "cp -r /home/$1/bashtools/.docker /home/$1/" $1
+    run su -c "mkdir -p /home/$1/work/dev1" $1
+
+    echo "Done"
+}
