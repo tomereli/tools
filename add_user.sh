63df445b066149fda29282235c30fa866dc9d52d
Tomer Eliyahu <tomer.b.eliyahu.com>
2020-07-13 18:49:40 +0300

    rename bashtools to tools
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu.com>

--- add_user.sh
+++ add_user.sh
@@ -30,14 +30,14 @@ confirm "Add user $1? [y/n]" && {
     run usermod -aG pcap $1
 
     run cd /home/$1
-    run su -c "git clone https://github.com/tomereli/bashtools.git /home/$1/bashtools" $1
-    run su -c "cp /home/$1/bashtools/.gitconfig /home/$1/" $1
-    run su -c "cp /home/$1/bashtools/.bashrc /home/$1/" $1
-    run su -c "cp /home/$1/bashtools/.bashrc.user /home/$1/.bashrc.$1" $1
-    run su -c "cp /home/$1/bashtools/.netrc /home/$1/" $1
-    run su -c "cp /home/$1/bashtools/.tmux.conf /home/$1/" $1
-    run su -c "cp /home/$1/bashtools/git-proxy /home/$1/" $1
-    run su -c "cp -r /home/$1/bashtools/.docker /home/$1/" $1
+    run su -c "git clone https://github.com/tomereli/tools.git /home/$1/tools" $1
+    run su -c "cp /home/$1/tools/.gitconfig /home/$1/" $1
+    run su -c "cp /home/$1/tools/.bashrc /home/$1/" $1
+    run su -c "cp /home/$1/tools/.bashrc.user /home/$1/.bashrc.$1" $1
+    run su -c "cp /home/$1/tools/.netrc /home/$1/" $1
+    run su -c "cp /home/$1/tools/.tmux.conf /home/$1/" $1
+    run su -c "cp /home/$1/tools/git-proxy /home/$1/" $1
+    run su -c "cp -r /home/$1/tools/.docker /home/$1/" $1
     run su -c "mkdir -p /home/$1/work/dev1" $1
 
     echo "Done"
